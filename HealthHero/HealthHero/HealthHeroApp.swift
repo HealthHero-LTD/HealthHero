ac90d8660ed1d6739ceb67ea32d7fcaa380bc188
Soroush Kami <soroushkami10@gmail.com>
2023-12-18 21:49:02 -0500

    create xcode skeleton

---
+++ HealthHero/HealthHero/HealthHeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthHeroApp.swift
+//  HealthHero
+//
+//  Created by soroush kami on 2023-12-18.
+//
+
+import SwiftUI
+
+@main
+struct HealthHeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
