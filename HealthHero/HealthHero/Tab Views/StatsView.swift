9406cf53107fb60cc14aaa0e3fcdf0a050fc45e0
Soroush Kami <soroushkami10@gmail.com>
2024-01-02 02:21:40 -0500

    create ViewExtensions

--- HealthHero/HealthHero/Tab Views/StatsView.swift
+++ HealthHero/HealthHero/Tab Views/StatsView.swift
@@ -9,7 +9,24 @@ import SwiftUI
 
 struct StatsView: View {
     var body: some View {
-        Text("stats!")
+        HStack {
+            
+            VStack{
+                Text("User's Level")
+                    .font(.title3)
+                Text("LVL 1")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            }
+            
+            VStack{
+                Text("Soroush_04")
+                    .font(.title2)
+            }
+        }
+        .padding(.top)
+        .background(.blue)
+//        .frame(maxHeight: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .top)
+        .alignmentInfinity(.top)
     }
 }
 
