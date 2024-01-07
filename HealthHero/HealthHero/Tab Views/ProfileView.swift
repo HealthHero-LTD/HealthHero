d060e1b8f985646afa5a5ce121117c40ab4608b6
Soroush Kami <soroushkami10@gmail.com>
2024-01-06 22:24:31 -0500

    create profile view

--- HealthHero/HealthHero/Tab Views/ProfileView.swift
+++ HealthHero/HealthHero/Tab Views/ProfileView.swift
@@ -9,7 +9,21 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        Text("profile")
+        
+        VStack {
+            Text("Soroush Kami")
+                .font(.title)
+            Text("@Soroush_04")
+        }
+        .frame(width: 400, height: 200)
+        .background(.green)
+        .padding()
+        
+        Spacer()
+        
+        VStack {
+            
+        }
     }
 }
 
