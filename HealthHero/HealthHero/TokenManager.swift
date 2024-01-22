a47832e5f17d1e8aec3dea4b28828c0741e4732e
Soroush Kami <soroushkami10@gmail.com>
2024-01-22 18:42:25 -0500

    implement TokenManager class

--- HealthHero/HealthHero/TokenManager.swift
+++ HealthHero/HealthHero/TokenManager.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+class TokenManager {
+    static let shared = TokenManager()
+    
+    func saveAccessToken(token accessToken: String) {
+        UserDefaults.standard.set(accessToken, forKey: "AccessTokenKey")
+    }
+    
+    func getAccessToken() -> String? {
+        return UserDefaults.standard.string(forKey: "AccessTokenKey")
+    }
+}
