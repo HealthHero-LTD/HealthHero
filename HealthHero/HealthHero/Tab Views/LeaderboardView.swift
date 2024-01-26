75d5e085bd94d903f4e127f4ab17ad1f6972a670
Soroush Kami <soroushkami10@gmail.com>
2024-01-26 14:26:12 -0500

    checkpoint for leaderboard UI

--- HealthHero/HealthHero/Tab Views/LeaderboardView.swift
+++ HealthHero/HealthHero/Tab Views/LeaderboardView.swift
@@ -9,23 +9,48 @@ import SwiftUI
 
 struct LeaderboardView: View {
     let leaderboardEntries = [
-        LeaderboardEntry(id: 1, username: "Player 1", level: 4, score: 100),
+        LeaderboardEntry(id: 1, username: "Player 1", level: 5, score: 100),
         LeaderboardEntry(id: 2, username: "Player 2", level: 4, score: 90),
-        LeaderboardEntry(id: 3, username: "Player 3", level: 2, score: 80),
-        LeaderboardEntry(id: 4, username: "Player 4", level: 3, score: 70),
+        LeaderboardEntry(id: 3, username: "Player 3", level: 3, score: 80),
+        LeaderboardEntry(id: 99, username: "Player 4", level: 2, score: 70),
     ]
+    
     var body: some View {
         VStack {
             Text("Leaderboard")
                 .font(.title)
                 .padding()
             
-            List(leaderboardEntries) { entry in
-                LeaderboardRow(entry: entry)
+            List {
+                HeaderRow()
+                ForEach(leaderboardEntries) { entry in
+                    LeaderboardRow(entry: entry)
+                }
             }
         }
     }
 }
+
+struct HeaderRow: View {
+    var body: some View {
+        HStack {
+            Text("#")
+                .fontWeight(.bold)
+                .frame(width: 25, alignment: .leading)
+            Text("Hero")
+                .fontWeight(.bold)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            Text("LVL")
+                .fontWeight(.bold)
+                .frame(width: 30, alignment: .trailing)
+            Text("Score")
+                .fontWeight(.bold)
+                .frame(width: 50, alignment: .trailing)
+        }
+        .padding(.horizontal)
+    }
+}
+
 
 struct LeaderboardRow: View {
     let entry: LeaderboardEntry
@@ -33,9 +58,11 @@ struct LeaderboardRow: View {
     var body: some View {
         HStack {
             Text("\(entry.id)")
-                .frame(width: 50, alignment: .leading)
+                .frame(width: 25, alignment: .leading)
             Text(entry.username)
                 .frame(maxWidth: .infinity, alignment: .leading)
+            Text("\(entry.level)")
+                .frame(width: 25, alignment: .leading)
             Text("\(entry.score)")
                 .frame(width: 50, alignment: .trailing)
         }
