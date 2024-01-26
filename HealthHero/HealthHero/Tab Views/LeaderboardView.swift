b78b2c9bbdd39951d411d8bdfe59d7c906f613e0
Soroush Kami <soroushkami10@gmail.com>
2024-01-26 14:05:47 -0500

    create a sample list for leaderboard

--- HealthHero/HealthHero/Tab Views/LeaderboardView.swift
+++ HealthHero/HealthHero/Tab Views/LeaderboardView.swift
@@ -8,11 +8,48 @@
 import SwiftUI
 
 struct LeaderboardView: View {
+    let leaderboardEntries = [
+        LeaderboardEntry(id: 1, username: "Player 1", level: 4, score: 100),
+        LeaderboardEntry(id: 2, username: "Player 2", level: 4, score: 90),
+        LeaderboardEntry(id: 3, username: "Player 3", level: 2, score: 80),
+        LeaderboardEntry(id: 4, username: "Player 4", level: 3, score: 70),
+    ]
     var body: some View {
-        Text("leaderboard")
+        VStack {
+            Text("Leaderboard")
+                .font(.title)
+                .padding()
+            
+            List(leaderboardEntries) { entry in
+                LeaderboardRow(entry: entry)
+            }
+        }
+    }
+}
+
+struct LeaderboardRow: View {
+    let entry: LeaderboardEntry
+    
+    var body: some View {
+        HStack {
+            Text("\(entry.id)")
+                .frame(width: 50, alignment: .leading)
+            Text(entry.username)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            Text("\(entry.score)")
+                .frame(width: 50, alignment: .trailing)
+        }
+        .padding(.horizontal)
     }
 }
 
 #Preview {
     LeaderboardView()
+}
+
+struct LeaderboardEntry: Identifiable {
+    let id: Int
+    let username: String
+    let level: Int
+    let score: Int
 }
