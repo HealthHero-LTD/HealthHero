e6242b6034ffc889e99e3e8510f93f6a872c92e1
Soroush Kami <soroushkami10@gmail.com>
2024-01-29 15:03:13 -0500

    change default tab view

--- HealthHero/HealthHero/Tab Views/MainView.swift
+++ HealthHero/HealthHero/Tab Views/MainView.swift
@@ -8,20 +8,25 @@
 import SwiftUI
 
 struct MainView: View {
+    @State private var selection = 2
+    
     var body: some View {
-        TabView {
+        TabView(selection: $selection) {
             LeaderboardView()
                 .tabItem {
                     Label("Leaderboard", systemImage: "trophy.circle")
                 }
+                .tag(0)
             StatsView()
                 .tabItem {
                     Label("Stats", systemImage: "list.bullet.clipboard.fill")
                 }
+                .tag(1)
             ProfileView()
                 .tabItem {
                     Label("Profile", systemImage: "person.crop.circle.fill")
                 }
+                .tag(2)
         }
     }
 }
