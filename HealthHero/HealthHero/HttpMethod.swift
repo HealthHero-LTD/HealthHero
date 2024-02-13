724dd0bcceafe6d2417d38d61db42d8be054b128
Mori Ahmadi <ahmadik.morteza95@gmail.com>
2024-02-13 10:30:51 -0500

    Add HttpMethod

---
+++ HealthHero/HealthHero/HttpMethod.swift
@@ -0,0 +1,16 @@
+//
+//  HttpMethod.swift
+//  HealthHero
+//
+//  Created by Mori Ahmadi on 2024-02-13.
+//
+
+import Foundation
+
+enum HttpMethod: String {
+    case GET
+    case POST
+    case PUT
+    case DELETE
+    case PATCH
+}
---
+++ HealthHero/HealthHero/HttpRequest.swift
@@ -0,0 +1,8 @@
+//
+//  HttpRequest.swift
+//  HealthHero
+//
+//  Created by Mori Ahmadi on 2024-02-13.
+//
+
+import Foundation
